36afc4fcb71eb623db548836e61a0d0ec1966da7
Angela Abdulina <despairrr@bk.ru>
2020-05-31 21:32:10 +0300

    createHospitalRow

--- script.js
+++ script.js
@@ -5,29 +5,53 @@ let promise = fetch(jsonPath)
     .then(hospitals => {
         let table = document.querySelector("table");
         hospitals.forEach(hospital => {
-            let tr = createRow([hospital.full_name, hospital.address, hospital.phone, ""]);
-            table.append(tr);
+            let tr = createHospitalRow(hospital);
+            table.tBodies[0].append(tr);
         })
     });
     
-function createElement(name) {
-    return document.createElement(name);
-}      
-let table = createElement("table");
+function createElement(name, options = {}) {
+    let element = document.createElement(name);
+    if (options.className) {
+        element.className = options.className;
+    }
+    return element;
+}    
+
+function createTable() {
+    let table = createElement("table", {className: "table table-hover"});
+    let thead = createElement("thead", {className: "thead-dark"});
+    let tbody = createElement("tbody");
+    table.append(thead);
+    table.append(tbody);
+    return table;
+}
+
+let table = createTable();
 let container = document.querySelector(".list");
 container.append(table);
 
-function createRow(content) {
+function createRow(content, options = {child:"td"}) {
     let tr = createElement("tr");
     content.forEach(copy => {
-        let td = createElement("td");
+        let td = createElement(options.child);
         td.innerText = copy;
         tr.append(td);
     });
     return tr;
 }
 
-let header = createRow(["name", "address", "phone", ""]);
-table.append(header);
+function createHospitalRow(hospital) {
+    let tr = createRow([hospital.full_name, hospital.address, hospital.phone, ""]);
+    let deleteButton = createElement("button", {className: "btn btn-dark"}); 
+    let editButton = createElement("button", {className: "btn btn-light mr-3"});
+    deleteButton.innerText = "Delete";
+    editButton.innerText = "Edit";
+    let lastChild = tr.children[tr.children.length - 1];
+    lastChild.append(editButton, deleteButton);
+    return tr;
+}
 
-console.log(1);
+let header = createRow(["name", "address", "phone", "actions"], {child: "th"});
+table.tHead.append(header);
+
